60d34f6ea952da7e58ab73e418f71e7ec207fd7c
Lhemingw <lhemingw@gmail.com>
2024-06-01 15:24:02 -0600

    Added Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
 // import React from 'react'
+import React, { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from './vite.svg'
+import './App.css'
+// import {useRoutes 
+//   Routes,
+//   Switch,
+//   Link,
+// } from 'react-router-dom';
+
+// import Home from './routes/Home'
+// import About from './routes/About'
+// import Finances from './routes/Finances'
+
+
+
 function App(){
+  // const routes = useRoutes([
+  //   { path: '/topics/:topicId', element:  },
+  //   ]);
+   
+  
   return(
     <>
+    
     <h1>Hello World</h1>
+
     </>
   )
+// } return routes;
 }
 export default App
